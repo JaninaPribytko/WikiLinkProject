63a4e03a15c94787aed165de0a2e3b002551a06f
JaninaPribytko <janina_pribytko@mail.ru>
2017-01-30 09:55:27 +0300

    First part

--- client/main.js
+++ client/main.js
@@ -1,32 +1,39 @@
+﻿import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-function onlyNum(evt) { 
-	var charCode = evt.which; 
-	if (charCode < 48 || charCode > 57) { 
-		return false; 
-	} 
-	return true; 
-}
 
-function onlyAB(evt){
-	var charCode = evt.which; //141 - 172
-	if ((charCode < 65 || charCode > 90) && (charCode <97 || charCode > 122)) { 
-		return false; 
-	} 
-	return true; 
-}
 
-function msg(){
-	var string = "Ваши данные приняты"; 
-	var flag = false;
+Template.BUTTON.onCreated(function helloOnCreated() {
+  this.counter = new ReactiveVar(true);
+});
+
+Template.SERIES.events({
+  'keypress input': function(event){
+    var charCode = event.which;
+    if ((charCode < 65 || charCode > 90) && (charCode <97 || charCode > 122)) { 
+      return false; 
+    } 
+    return true; 
+  }
+});
+
+Template.NUMBER.events({
+  'keypress input': function(event){
+    var charCode = event.which;
+    if (charCode < 48 || charCode > 57) { 
+      return false; 
+    } 
+    return true;
+  }
+});
+
+Template.BUTTON.events({
+  'click button'(event, instance) {
+  var flag = false;
 	var lettersField = document.getElementById("field_of_letters").value;
 	var numbersField = document.getElementById("field_of_numbers").value;
 	if (lettersField.length === 2 && numbersField.length === 7) {
-		flag = true;
-	}
-	if (flag) {
-		document.getElementById("field_of_letters").value = "";
-		document.getElementById("field_of_numbers").value = "";
-	 	alert(string);
-	 }
-}
+		alert("Ваши данные приняты c:");
+}	}
+});
