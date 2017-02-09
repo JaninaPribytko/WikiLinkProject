3f830552452999f2a70feca27b9e0718f55d8ebc
JaninaPribytko <janina_pribytko@mail.ru>
2017-02-10 01:49:14 +0300

    Replace alert

--- client/main.js
+++ client/main.js
@@ -23,17 +23,29 @@ Template.SERIES.events({
 Template.BUTTON.events({ 
   'click button'(event, instance) { 
     var lettersField = document.getElementById("field_of_letters").value; 
+    var resultLab = document.getElementById("resultLable"); 
     if (lettersField.length === 9){
      	if (/\D\D\d\d\d\d\d\d\d/.test(lettersField)){
      		event.preventDefault(); 
     		Meteor.call('checkData', lettersField.toUpperCase(), function (error, result) { 
-    		if(result) 
-      			alert("Данные правильные"); 
-    		else 
-      			alert("Данные неверные");
+    		if(result){
+      			resultLab.innerHTML='Ваши данные приняты';
+      			resultLab.style.border='2px solid #32a32e';
+				resultLab.style.display='inline';
+				setTimeout(function(){resultLab.style.display="none";}, 1000);
+    		}
+    		else {
+      				resultLab.innerHTML='Данные неверные';
+      				resultLab.style.border='2px solid #FF5353';
+					resultLab.style.display='inline';
+				}
+
       		}); 
 		} 
-		else  {alert("Неверный формат. Ввод в формате AB1234567");} 
+		else{	resultLab.innerHTML='Нeверный формат';
+				resultLab.style.border='2px solid #FF5353';
+				resultLab.style.display='inline';
+			}
 	document.getElementById("field_of_letters").value=""; 
 	}
  }
